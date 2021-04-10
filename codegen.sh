518c08b8205d842100ac81e7a7fdf4e5c85443e6
htchepannou <htchepannou@expedia.com>
2021-04-10 18:10:45 -0400

    update configuration

--- codegen.sh
+++ codegen.sh
@@ -1,14 +1,24 @@
-java -jar ../wutsi-codegen/target/wutsi-codegen-0.0.24.jar server \
-    -in https://wutsi-openapi.s3.amazonaws.com/telegram_api.yaml \
+#/bin/sh
+
+CODEGEN_VERSION="0.0.27"
+CODEGEN_JAR=~/wutsi-codegen/wutsi-codegen-${CODEGEN_VERSION}.jar
+
+API_NAME=telegram
+API_URL=https://wutsi-openapi.s3.amazonaws.com/${API_NAME}_api.yaml
+GITHUB_USER=wutsi
+
+echo "Generating code from ${API_URL}"
+java -jar ${CODEGEN_JAR} server \
+    -in ${API_URL} \
     -out . \
-    -name telegram \
-    -package com.wutsi.telegram \
+    -name ${API_NAME} \
+    -package com.wutsi.${API_NAME} \
     -jdk 11 \
-    -github_user wutsi \
-    -github_project telegram-server \
-    -heroku wutsi-telegram \
-    -service_database \
+    -github_user ${GITHUB_USER} \
+    -github_project ${API_NAME}-sdk-kotlin \
+    -heroku ${API_NAME}-site \
     -service_logger \
+    -service_database \
     -service_mqueue
 
 if [ $? -eq 0 ]
