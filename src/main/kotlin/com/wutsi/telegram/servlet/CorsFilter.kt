2d64778c426fae84552d3072fb1fe0cab2f4675b
htchepannou <htchepannou@expedia.com>
2021-05-05 12:27:17 -0400

    update dependencies

--- src/main/kotlin/com/wutsi/telegram/servlet/CorsFilter.kt
+++ src/main/kotlin/com/wutsi/telegram/servlet/CorsFilter.kt
@@ -23,7 +23,7 @@ public class CorsFilter : Filter {
         )
         resp.addHeader(
             "Access-Control-Allow-Headers",
-            "Content-Type, Authorization, Content-Length,X-Requested-With"
+            "Content-Type, Authorization, Content-Length,X-Requested-With,Authorization"
         )
         chain.doFilter(req, resp)
     }
